18d627403e8c20bc2ceabe8773996b58eda3985d
Denis Kalkopf <ddenproton@protonmail.com>
2025-03-12 16:34:58 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div>
+      <header className="header">
+        <h1>Portfolio Overview</h1>
+        <button type="button">Добавить</button>
+      </header>
+      <div className="board">Board</div>
+    </div>
+  );
+}
+
+export default App;
